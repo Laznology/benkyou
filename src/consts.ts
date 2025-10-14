70d9e21452e6dc082345f689e5329b387e63bd24
LazTech <laznology.dev@gmail.com>
2025-10-14 14:56:19 +0700

    feat: update site constants for new site identity and social links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Hinode";
+export const SITE_DESCRIPTION = "A Static Blog";
+export const GITHUB_URL = import.meta.env.PUBLIC_GITHUB_URL;
+export const LINKEDIN_URL = import.meta.env.PUBLIC_LINKEDIN_URL;
